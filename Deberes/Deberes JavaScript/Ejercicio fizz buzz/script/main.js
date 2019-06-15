0af3cb536aee7afce8980685445427ee2bad3fb2
victoria <victoriamunozsisso@gmail.com>
2019-06-15 18:04:01 +0200

    respaldo

---
+++ Deberes/Deberes JavaScript/Ejercicio fizz buzz/script/main.js
@@ -0,0 +1,9 @@
+var bienvenida="Bienvenido a mi web";
+console.log(bienvenida);
+var pregunta="Cual es tu nombre";
+var nombreTexy=prompt(pregunta);
+var nombreVictoria=prompt(pregunta);
+var nombreAntonio=prompt(pregunta);
+alert(nombreTexy);
+alert(nombreVictoria);
+alert(nombreAntonio);
